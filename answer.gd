a4ada00f478572d7f0e03e8e7c861fccb51c0d3b
BerlynSG <135674997+BerlynSG@users.noreply.github.com>
2025-03-27 18:36:56 -0400

    Cambiar nombre de algunas variables

--- answer.gd
+++ answer.gd
@@ -4,22 +4,22 @@ var name: String
 var question: Question
 
 var x: int = 0
-var frecuency: int = 0
-var frecuency_relative: float
-var frecuency_porcent: float
-var frecuency_absolute: int
+var frecuencia: int = 0
+var frecuencia_relativa: float
+var frecuencia_porcentual: float
+var frecuencia_acumulada: int
 
 var xf: int
 
 func _init(name: String, question: Question) -> void:
 	self.name = name
 	self.question = question
-	frecuency = 1
+	frecuencia = 1
 	
 
-func add_frecuency():
-	frecuency += 1
+func add_frecuencia():
+	frecuencia += 1
 
 func calculate():
-	frecuency_relative = float(frecuency) / question.frecuency_total
-	frecuency_porcent = frecuency_relative * 100
+	frecuencia_relativa = float(frecuencia) / question.frecuencia_total
+	frecuencia_porcentual = frecuencia_relativa * 100
--- question.gd
+++ question.gd
@@ -2,7 +2,7 @@ class_name Question
 
 var answers: Dictionary[String, Answer]
 
-var frecuency_total: int
+var frecuencia_total: int
 var xf_total: int
 var media: float
 var mediana1: int
@@ -21,6 +21,8 @@ func get_table() -> String:
 	text += "[cell]Optiones[/cell][cell]|x[/cell]
 	[cell]|f[/cell][cell]|fr[/cell][cell]|fp[/cell][cell]|F[/cell][cell]|xf[/cell]"
 	var n = 1
+	[cell]|f[/cell][cell]|fr[/cell][cell]|fp[/cell][cell]|F[/cell]"
+	#var n = 1
 	var fr_total: float = 0
 	var fp_total: float = 0
 	for a in answers:
@@ -28,14 +30,10 @@ func get_table() -> String:
 		text += "[cell]|" + str(n) + "[/cell]"
 		n += 1
 		text += "[cell]|" + str(answers[a].frecuency) + "[/cell]"
-		text += "[cell]|" + String.num(answers[a].frecuency_relative, 2) + "[/cell]"
-		fr_total += answers[a].frecuency_relative
-		text += "[cell]|" + String.num(answers[a].frecuency_porcent, 2) + "%[/cell]"
-		fp_total += answers[a].frecuency_porcent
-		text += "[cell]|" + str(answers[a].frecuency_absolute) + "[/cell]"
-		text += "[cell]|" + str(answers[a].xf) + "[/cell]"
+		text += "[cell]|" + str(answers[a].frecuencia) + "[/cell]"
+		text += "[cell]|" + String.num(answers[a].frecuencia_relativa, 2) + "[/cell]"
 	text += "[cell][/cell][cell]|[/cell]
-	[cell]|" + str(frecuency_total) + "[/cell]
+	[cell]|" + str(frecuencia_total) + "[/cell]
 	[cell]|" + str(fr_total) + "[/cell][cell]|" + str(fp_total) + "%[/cell]]
 	[cell]|[/cell][cell]|" + str(xf_total) + "[/cell]"
 	text += "[/table]"
@@ -47,44 +45,44 @@ func get_table() -> String:
 	return text
 
 func calculate():
-	set_frecuency_total()
+	set_frecuencia_total()
 	for a in answers:
 		answers[a].calculate()
-	if frecuency_total != 0:
-		media = float(xf_total) / frecuency_total
 	set_mediana()
 	set_moda()
+	if frecuencia_total != 0:
+		media = float(xf_total) / frecuencia_total
 
-func set_frecuency_total():
 	var x: int = 1
+func set_frecuencia_total():
 	for a in answers:
 		var answer: Answer = answers[a]
-		frecuency_total += answer.frecuency
-		answer.frecuency_absolute = frecuency_total
-		answer.x = x
-		answer.xf = x * answer.frecuency
-		xf_total += answer.xf
-		x += 1
+		frecuencia_total += answer.frecuencia
+		answer.frecuencia_acumulada = frecuencia_total
+		#answer.x = x
+		#answer.xf = x * answer.frecuencia
+		#xf_total += answer.xf
+		#x += 1
 
 func set_mediana():
 	var pos = 0
-	if frecuency_total % 2 == 0:
-		pos = frecuency_total/2
+	if frecuencia_total % 2 == 0:
+		pos = frecuencia_total/2
 		var x1 = 0
 		var x2 = 0
 		for a in answers:
-			if pos <= answers[a].frecuency_absolute && x1 == 0:
+			if pos <= answers[a].frecuencia_acumulada && x1 == 0:
 				x1 = answers[a].x
-			if pos <= answers[a].frecuency_absolute && x2 == 0:
+			if pos <= answers[a].frecuencia_acumulada && x2 == 0:
 				x2 = answers[a].x
 			if x1 != 0 && x2 != 0: break
 		mediana1 = x1
 		mediana2 = x2
 	else:
-		pos = (frecuency_total + 1)/2
+		pos = (frecuencia_total + 1)/2
 		var x = 0
 		for a in answers:
-			if pos <= answers[a].frecuency_absolute && x == 0:
+			if pos <= answers[a].frecuencia_acumulada && x == 0:
 				x = answers[a].x
 			if x != 0: break
 		mediana1 = x
@@ -93,6 +91,6 @@ func set_mediana():
 func set_moda():
 	var m = 0
 	for a in answers.size():
-		if answers[answers.keys()[m]].frecuency < answers[answers.keys()[a]].frecuency:
+		if answers[answers.keys()[m]].frecuencia < answers[answers.keys()[a]].frecuencia:
 			m = a
 	moda = m + 1
