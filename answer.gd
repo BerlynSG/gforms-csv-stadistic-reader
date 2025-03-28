0ee78f577e902ef292233577399aa577c0373547
BerlynSG <135674997+BerlynSG@users.noreply.github.com>
2025-03-28 12:04:23 -0400

    Quitar cosas innecesarias
    Quitar variables y funciones innecesarias

--- answer.gd
+++ answer.gd
@@ -3,19 +3,15 @@ class_name Answer
 var name: String
 var question: Question
 
-var x: int = 0
 var frecuencia: int = 0
 var frecuencia_relativa: float
 var frecuencia_porcentual: float
 var frecuencia_acumulada: int
 
-var xf: int
-
 func _init(name: String, question: Question) -> void:
 	self.name = name
 	self.question = question
 	frecuencia = 1
-	
 
 func add_frecuencia():
 	frecuencia += 1
--- question.gd
+++ question.gd
@@ -3,11 +3,6 @@ class_name Question
 var answers: Dictionary[String, Answer]
 
 var frecuencia_total: int
-var xf_total: int
-var media: float
-var mediana1: int
-var mediana2: int
-var moda: int
 
 func add_answer(name: String):
 	if answers.has(name):
@@ -32,49 +27,9 @@ func calculate():
 	set_frecuencia_total()
 	for a in answers:
 		answers[a].calculate()
-	set_mediana()
-	set_moda()
-	if frecuencia_total != 0:
-		media = float(xf_total) / frecuencia_total
 
-	var x: int = 1
 func set_frecuencia_total():
 	for a in answers:
 		var answer: Answer = answers[a]
 		frecuencia_total += answer.frecuencia
 		answer.frecuencia_acumulada = frecuencia_total
-		#answer.x = x
-		#answer.xf = x * answer.frecuencia
-		#xf_total += answer.xf
-		#x += 1
-
-func set_mediana():
-	var pos = 0
-	if frecuencia_total % 2 == 0:
-		pos = frecuencia_total/2
-		var x1 = 0
-		var x2 = 0
-		for a in answers:
-			if pos <= answers[a].frecuencia_acumulada && x1 == 0:
-				x1 = answers[a].x
-			if pos <= answers[a].frecuencia_acumulada && x2 == 0:
-				x2 = answers[a].x
-			if x1 != 0 && x2 != 0: break
-		mediana1 = x1
-		mediana2 = x2
-	else:
-		pos = (frecuencia_total + 1)/2
-		var x = 0
-		for a in answers:
-			if pos <= answers[a].frecuencia_acumulada && x == 0:
-				x = answers[a].x
-			if x != 0: break
-		mediana1 = x
-		mediana2 = x
-
-func set_moda():
-	var m = 0
-	for a in answers.size():
-		if answers[answers.keys()[m]].frecuencia < answers[answers.keys()[a]].frecuencia:
-			m = a
-	moda = m + 1
